ad691e6a8a96e7f1aa8ff2e538cf7b3cdf94a3eb
Victor Nguyen <victornguyen082@gmail.com>
2020-04-30 20:18:54 -0400

    created starter navigation and hero function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
+
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:id" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }

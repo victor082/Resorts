9f341f84a48eccfc5eb79a81d7a7f1b246cc5d1b
Victor Nguyen <victornguyen082@gmail.com>
2020-05-02 18:21:57 -0400

    creating title subtitle and children banner for the error, home, and rooms page

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 const Rooms = () => {
     return (
-        <Hero hero="roomsHero" />
+        <Hero hero="roomsHero">
+            <Banner title="Our Rooms">
+                <Link to="/rooms" className="btn-primary">return home</Link>
+            </Banner>
+        </Hero>
     )
 }
 export default Rooms

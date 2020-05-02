3dccd3293d73e904ffafe003de9054493aa75353
Victor Nguyen <victornguyen082@gmail.com>
2020-05-02 19:41:24 -0400

    created featured room section and a loading gif

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import { RoomContext } from '../Context'
+import Room from './Room'
+import Loading from './Loading'
+import Title from './Title'
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-        const { name, greeting } = this.context
+        let { loading, featuredRooms: rooms } = this.context
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
         return (
-            <div>
-
-            </div>
+            <section className="featured-rooms">
+                <Title title="Featured Rooms"></Title>
+                <div className="featured-rooms-center">
+                    {loading ? <Loading /> : rooms}
+                </div>
+                <Room />
+            </section>
         )
     }
 }
